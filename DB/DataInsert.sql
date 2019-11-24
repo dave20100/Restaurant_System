e030aca42feb753eef9b75e7db4dbd1a1849be77
Dawid <dave201000@gmail.com>
2019-11-24 16:51:17 +0100

    added datainsert file

---
+++ DB/DataInsert.sql
@@ -0,0 +1,14 @@
+--INSERT INTO Location(Id, Street, Number, City, PostCode) VALUES
+--(1, 'CC', 12, '123', '3432234');
+
+--INSERT INTO Restaurant(Id, Name, Description) VALUES
+--(1, 'ADSA', 'ASDFA');
+
+--INSERT INTO Menu(Id, Description, ResteurantId) VALUES
+--(1, 'ASD', 1);
+
+--INSERT INTO Dish(Id, Description, Price, Kcal, Picture, MenuId) VALUES
+--(1, 'OPIS', 12, 232, CAST('IMG' AS varbinary));
+
+--INSERT INTO Discount(Id, Description, Value, ResteurantId) VALUES
+--(1, 'EQWEQE', 1231, 12);
